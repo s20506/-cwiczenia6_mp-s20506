e5bafd119454b2de6e588309e8d3ab64967e673b
Oskar Kupski <oskar.kupski@gmail.com>
2022-06-11 13:17:56 +0200

    added data seed

---
+++ App/Migrations/20220611111651_AddedSeeds.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace App.Migrations
+{
+    public partial class AddedSeeds : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Doctors",
+                columns: new[] { "IdDoctor", "Email", "FirstName", "LastName" },
+                values: new object[] { 1, "jan@example.com", "Jan", "Kowalski" });
+
+            migrationBuilder.InsertData(
+                table: "Medicaments",
+                columns: new[] { "IdMedicament", "Description", "Name", "Type" },
+                values: new object[] { 1, "Lek na bol glowy", "Apap", "tabletka" });
+
+            migrationBuilder.InsertData(
+                table: "Patients",
+                columns: new[] { "IdPatient", "Birthdate", "FirstName", "LastName" },
+                values: new object[] { 1, new DateTime(2000, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Jan", "Nowak" });
+
+            migrationBuilder.InsertData(
+                table: "Prescriptions",
+                columns: new[] { "IdPrescription", "Date", "DueDate", "IdDoctor", "IdPatient" },
+                values: new object[] { 1, new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, 1 });
+
+            migrationBuilder.InsertData(
+                table: "PrescriptionMedicaments",
+                columns: new[] { "IdMedicament", "IdPrescription", "Details", "Dose" },
+                values: new object[] { 1, 1, "rano", 1 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "PrescriptionMedicaments",
+                keyColumns: new[] { "IdMedicament", "IdPrescription" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "Medicaments",
+                keyColumn: "IdMedicament",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Prescriptions",
+                keyColumn: "IdPrescription",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Doctors",
+                keyColumn: "IdDoctor",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Patients",
+                keyColumn: "IdPatient",
+                keyValue: 1);
+        }
+    }
+}
--- App/Models/MainDbContext.cs
+++ App/Models/MainDbContext.cs
@@ -22,6 +22,36 @@ public class MainDbContext : DbContext
     {
         base.OnModelCreating(modelBuilder);
 
-        modelBuilder.Entity<PrescriptionMedicament>().HasKey(e => new {e.IdMedicament, e.IdPrescription});
+        modelBuilder.Entity<Doctor>(p =>
+        {
+            p.HasData(new Doctor()
+                {IdDoctor = 1, FirstName = "Jan", LastName = "Kowalski", Email = "jan@example.com"});
+        });
+        modelBuilder.Entity<Medicament>(p =>
+        {
+            p.HasData(new Medicament()
+                {IdMedicament = 1, Name = "Apap", Description = "Lek na bol glowy", Type = "tabletka"});
+        });
+        modelBuilder.Entity<Patient>(p =>
+        {
+            p.HasData(new Patient()
+                {IdPatient = 1, FirstName = "Jan", LastName = "Nowak", Birthdate = DateTime.Parse("2000-01-01")});
+        });
+        modelBuilder.Entity<Prescription>(p =>
+        {
+            p.HasData(new Prescription()
+            {
+                IdPrescription = 1, Date = DateTime.Parse("2022-01-01"), DueDate = DateTime.Parse("2023-01-01"),
+                IdPatient = 1, IdDoctor = 1
+            });
+        });
+        modelBuilder.Entity<PrescriptionMedicament>(p =>
+        {
+            p.HasKey(e => new {e.IdMedicament, e.IdPrescription});
+            p.HasData(new PrescriptionMedicament()
+            {
+                IdMedicament = 1, IdPrescription = 1, Dose = 1, Details = "rano"
+            });
+        });
     }
 }
